eb57ef26c5e2973d2468489d4bc161ff77e2ee68
Mena1800 <cmena0102@gmail.com>
2024-01-29 16:19:08 -0600

    cmabios en tarea 1 29/01/2024 04:18pm

--- Tarea 1 Programacion 2/Program.cs
+++ Tarea 1 Programacion 2/Program.cs
@@ -7,16 +7,27 @@ int tipoempleado = 0;
 int cantidadhoras = 0;
 int preciohora = 0;
 
-Console.WriteLine("Bienvendido al programa de aumentos salariales de Tienda la Baratica SA");
-Console.WriteLine("Digite su numero de cedula (sin guiones ni espacios): ");
-cedula = int.Parse(Console.ReadLine());
-Console.WriteLine("Digite su nombre completo: ");
-nombre = Console.ReadLine();
-Console.WriteLine("Digite su tipo de empleado: (1-Operario 2-Tecnico 3-Profesional)");
+
 do
 {
-	if (true)
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Bienvendido al programa de aumentos salariales de Tienda la Baratica SA");
+    Console.WriteLine("Digite su numero de cedula (sin guiones ni espacios): ");
+    cedula = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite su nombre completo: ");
+    nombre = Console.ReadLine();
+    Console.WriteLine("Digite su tipo de empleado: (1-Operario 2-Tecnico 3-Profesional)");
+    if (tipoempleado == 1)
 	{
 
-	}
-} while (true);
+    }
+    else if (tipoempleado == 2)
+    {
+
+    }
+    else if (tipoempleado == 3)
+    {
+
+    }
+} while (tipoempleado <1 || tipoempleado > 3);
