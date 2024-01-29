9a4b109e91dbbd5996bc5d8252fbfd452162a9e0
Mena1800 <cmena0102@gmail.com>
2024-01-29 16:14:18 -0600

    Agregar archivos de proyecto.

---
+++ Tarea 1 Programacion 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int cedula = 0;
+string nombre = "";
+int tipoempleado = 0;
+int cantidadhoras = 0;
+int preciohora = 0;
+
+Console.WriteLine("Bienvendido al programa de aumentos salariales de Tienda la Baratica SA");
+Console.WriteLine("Digite su numero de cedula (sin guiones ni espacios): ");
+cedula = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite su nombre completo: ");
+nombre = Console.ReadLine();
+Console.WriteLine("Digite su tipo de empleado: (1-Operario 2-Tecnico 3-Profesional)");
+do
+{
+	if (true)
+	{
+
+	}
+} while (true);
